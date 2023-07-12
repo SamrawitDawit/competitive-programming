53cbdcbdfd30db6f00239ba5e09bdd2128229eb2
SamrawitDawit <115358870+SamrawitDawit@users.noreply.github.com>
2023-07-13 02:13:34 +0300

    fizz_buzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,14 +1,15 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        x = []
-        for i in range(1,n+1):
-            if i%15==0:
-               x.append("FizzBuzz")
-            elif i%3==0:
-               x.append("Fizz")
-            elif i%5==0:
-               x.append("Buzz")
-            else:
-               x.append(str(i))
-        return x
+       Array = [] 
+       for i in range(1,n+1):
+           if i%3 == 0 and i%5 == 0:
+               p = ("FizzBuzz")
+           elif i%3 == 0:
+               p = ("Fizz")
+           elif i%5 == 0:
+               p = ("Buzz")
+           else:
+               p = str(i)
+           Array.append(p)
+       return Array
         
