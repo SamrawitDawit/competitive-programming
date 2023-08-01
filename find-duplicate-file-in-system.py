91fcbe0b2b79fefc8a08c4c1fa8ec6f7fc2b8c4a
SamrawitDawit <115358870+SamrawitDawit@users.noreply.github.com>
2023-08-02 02:13:18 +0300

    Create find-duplicate-file-in-system.py

---
+++ find-duplicate-file-in-system.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findDuplicate(self, paths: List[str]) -> List[List[str]]:
+        directory_dic = collections.defaultdict(list)
+        for i in paths:
+            j = i.split(" ")
+            for k in j[1:]:
+                directory_dic[k.split("(")[1]].append(j[0]+"/"+k.split("(")[0])
+        answer = []
+        for i in directory_dic.values():
+            if len(i) > 1:
+                answer.append(i)
+        return answer
