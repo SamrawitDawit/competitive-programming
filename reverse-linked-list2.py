41b1955824324d4dbd0fd370e5fb49a854d05483
SamrawitDawit <115358870+SamrawitDawit@users.noreply.github.com>
2023-09-08 19:05:14 +0300

    Create reverse-linked-list2.py

---
+++ reverse-linked-list2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        if left == right:
+            return head
+        count = 1
+        dummy = ListNode(0)
+        dummy.next = head
+        prev = dummy
+        while count != left:
+            prev = prev.next 
+            count += 1
+        previous = prev     
+        curr = prev.next
+        leftN = curr
+        while count < right+1:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+            count += 1
+        previous.next = prev
+        leftN.next = curr
+        return dummy.next
