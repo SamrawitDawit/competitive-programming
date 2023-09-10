fd673aa557636caa0965dea1d490ab022dbf43ed
SamrawitDawit <115358870+SamrawitDawit@users.noreply.github.com>
2023-09-10 16:01:42 +0300

    Create permutation-in-string.py

---
+++ permutation-in-string.py
@@ -0,0 +1,14 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1_dic = collections.Counter(s1)
+        s2_dic = collections.Counter(s2[:len(s1)])
+        l = 0
+        for r in range(len(s1),len(s2)):
+            if s1_dic == s2_dic:
+                return True
+            s2_dic[s2[l]] -= 1
+            s2_dic[s2[r]] += 1
+            l += 1
+        if s1_dic == s2_dic:
+            return True
+        return False
