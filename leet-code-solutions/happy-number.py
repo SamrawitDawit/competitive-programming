75335eae12334612716eb2b45d930b5f783b2091
SamrawitDawit <115358870+SamrawitDawit@users.noreply.github.com>
2023-12-12 23:50:51 +0300

    Added solution for Happy Number

--- leet-code-solutions/happy-number.py
+++ leet-code-solutions/happy-number.py
@@ -2,12 +2,11 @@ class Solution:
     def isHappy(self, n: int) -> bool:
         i = 0
         n = str(n)
-        while i < 20:
+        while i < 7:
             summ = 0
             for letter in n:
                 summ += (int(letter)**2)
                 n = str(summ)
-                print(summ)
             if summ == 1:
                 return True
             i += 1
