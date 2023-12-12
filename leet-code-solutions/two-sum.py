323f7123ef5ad4565202e659d09f08dfa4700ee7
SamrawitDawit <115358870+SamrawitDawit@users.noreply.github.com>
2023-12-12 15:52:46 +0300

    Added solution for Two Sum

---
+++ leet-code-solutions/two-sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+        for i, n in enumerate(nums):
+            dif = target - n
+            if dif in dic:
+                return [dic[dif], i]
+            else:
+                dic[n] = i
+            
+        
+        
+
