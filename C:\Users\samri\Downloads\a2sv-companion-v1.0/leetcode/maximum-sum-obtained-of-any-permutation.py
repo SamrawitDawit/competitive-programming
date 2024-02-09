708ebfa97042d4ca3b38aeaf9024844ea8bcc708
SamrawitDawit <115358870+SamrawitDawit@users.noreply.github.com>
2024-02-09 04:28:19 +0300

    Add solution for Maximum Sum Obtained of Any Permutation

---
+++ C:\Users\samri\Downloads\a2sv-companion-v1.0/leetcode/maximum-sum-obtained-of-any-permutation.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxSumRangeQuery(self, nums: List[int], requests: List[List[int]]) -> int:
+        freq_indices = [0]*len(nums)
+        for request in requests:
+            freq_indices[request[0]] += 1
+            if request[1] < len(nums)-1:
+                freq_indices[request[1]+1] -= 1
+        prefix = 0
+        for i in range(len(nums)):
+            prefix += freq_indices[i]
+            freq_indices[i] = prefix
+        print(sorted(freq_indices))
+        print(sorted(nums))
+        return sum(num * freq for num, freq in zip(sorted(nums), sorted(freq_indices))) % (10 ** 9 + 7)
+        
+     
+        
+
+            
