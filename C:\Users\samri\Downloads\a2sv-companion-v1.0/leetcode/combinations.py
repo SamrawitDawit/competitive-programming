2550c256e14f79a8b2a08380c5e2895f80e5cacf
SamrawitDawit <115358870+SamrawitDawit@users.noreply.github.com>
2024-02-26 22:37:41 +0300

    Add solution for Combinations

---
+++ C:\Users\samri\Downloads\a2sv-companion-v1.0/leetcode/combinations.py
@@ -0,0 +1,15 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        def backtrack(start, path):
+            if len(path) == k:
+                ans.append(path.copy())
+                return 
+            for i in range(start, n+1):
+                path.append(i)
+                backtrack(i+1, path)
+                path.pop()
+        ans = []
+        backtrack(1, [])
+        return ans
+   
+        
