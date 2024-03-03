59c2631f5dbd03f972b111beb8272f7645b3d4f9
SamrawitDawit <115358870+SamrawitDawit@users.noreply.github.com>
2024-03-03 15:35:37 +0300

    Add solution for Minimum Replacements to Sort the Array

---
+++ C:\Users\samri\Downloads\a2sv-companion-v1.0/leetcode/minimum-replacements-to-sort-the-array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minimumReplacement(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return 0
+        count = 0
+        for i in range(len(nums)-2, -1, -1):
+            if nums[i] > nums[i+1]:
+                space = ceil(nums[i]/nums[i+1])
+                count += (space-1)
+                nums[i] = nums[i]//space
+                print(space, ceil(space/2))
+        return count
