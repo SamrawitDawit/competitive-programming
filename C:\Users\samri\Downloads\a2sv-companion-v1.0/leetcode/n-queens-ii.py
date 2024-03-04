d83eec9c322c07103bd20a57570b4579f8e3d355
SamrawitDawit <115358870+SamrawitDawit@users.noreply.github.com>
2024-03-04 12:59:07 +0300

    Add solution for N-Queens II

---
+++ C:\Users\samri\Downloads\a2sv-companion-v1.0/leetcode/n-queens-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        col_set, sum_diagonal, diff_diagonal = set(), set(), set()
+        ans = []
+        puzzle = [['.']*n for row in range(n)]
+        def backtrack(r):
+            if r == n:
+                ans.append(puzzle)
+                return
+            for c in range(n):
+                if c in col_set or r+c in sum_diagonal or r-c in diff_diagonal:
+                    continue
+                col_set.add(c)
+                sum_diagonal.add(r+c)
+                diff_diagonal.add(r-c)
+                puzzle[r][c] = 'Q'
+                backtrack(r+1)
+                col_set.remove(c)
+                sum_diagonal.remove(r+c)
+                diff_diagonal.remove(r-c)
+                puzzle[r][c] = '.'
+        backtrack(0)
+        return len(ans)
